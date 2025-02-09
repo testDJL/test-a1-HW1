06e2319391f365a6500550a4083fe367161fc373
dnjgld <57654573+dnjgld@users.noreply.github.com>
2025-02-08 19:21:16 -0500

    Update test_problem_1.py

--- test_problem_1.py
+++ test_problem_1.py
@@ -1,5 +1,3 @@
 from main import *
 
-def test_myName():
-    # Check if the result of myName() is a string
-    assert isinstance(myName(), str), "myName() should return a string"
+assert isinstance(myName(), str), "myName() should return a string"
